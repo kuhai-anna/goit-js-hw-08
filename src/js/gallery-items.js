c6fe72d7458158bcb7558c08a6498f567155d3fa
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-02 00:25:07 +0200

    Update gallery-items.js
    Add more gallery elements

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -62,4 +62,67 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/03/15/13/27/rough-horn-2146181__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/03/15/13/27/rough-horn-2146181_1280.jpg',
+    description: 'Rough Horn Alpine',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg',
+    description: 'Sunrise Boat',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/06/13/18/20/waves-3473335__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/06/13/18/20/waves-3473335_1280.jpg',
+    description: 'Waves Sea',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/03/27/15/14/forest-2179318__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/03/27/15/14/forest-2179318_1280.jpg',
+    description: 'Forest Path',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/06/04/17/23/flowers-3453729__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/06/04/17/23/flowers-3453729_1280.jpg',
+    description: 'Flowers Grape Hyacinths',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204_1280.jpg',
+    description: 'Alberta Canada',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2013/02/21/19/10/sea-84629__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2013/02/21/19/10/sea-84629_1280.jpg',
+    description: 'Sea Horizon',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_1280.jpg',
+    description: 'Boat Lake',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/04/29/01/22/tea-plantation-3358870__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/04/29/01/22/tea-plantation-3358870_1280.jpg',
+    description: 'Tea Plantation Landscape',
+  },
 ];
