7c0fa2e25589686d41bd69dd46b8b289f29ad57e
KuhaiAnna <kuhai.anna@gmail.com>
2023-03-04 21:56:39 +0200

    Add code for task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+const TIME_KEY = 'videoplayer-current-time';
+
+// Викликається при завантаженні або перезавантаженні сторінки
+setCurrentTime();
+
+// Виклик функції оновлення часу, частота запису даних у сховище
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// Отримуємо час відеоплеєра
+// Записуємо його у сховище
+function onTimeUpdate() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(TIME_KEY, seconds);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
+}
+
+// Отримуємо дані зі сховища
+// Якщо там щось є, оновлюємо час
+function setCurrentTime() {
+  player.setCurrentTime(localStorage.getItem(TIME_KEY) || 0);
+}
